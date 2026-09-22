57566199d60fa819395930cf8f7ca136d0a976ad
albi <opodark@gmail.com>
2026-09-22 21:52:55 +0200

    feat(web-node): add public mesh join page

---
+++ web-node/join-config.js
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copia questo file insieme a join.html e src/ sullo spazio web Aruba.
+// L'endpoint deve essere il gateway pubblico HTTPS, mai il control-plane.
+window.HYPERSPACE_JOIN = Object.freeze({
+  gatewayUrl: "https://mesh.zerozerocomputer.it",
+  siteName: "ZeroZeroComputer",
+  meshName: "HyperSpace",
+});
